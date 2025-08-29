2ca68f39c3571a0622ca4931e3b97f22165558cf
jared <harishussain.hashone@gmail.com>
2025-08-29 21:13:47 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/nestjs-api', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nestjs-api'),
     UsersModule,
   ],
   controllers: [AppController],
